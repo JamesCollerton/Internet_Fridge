5304ade10ba763b40841e0c769397ca648206708
James Collerton <jamescollerton@live.co.uk>
2016-02-21 10:42:09 +0000

    Pushing before I get rid of existing code in template

--- Internet_Fridge_App/Internet_Fridge/app/src/main/java/com/example/jamescollerton/internet_fridge/HomeScreen.java
+++ Internet_Fridge_App/Internet_Fridge/app/src/main/java/com/example/jamescollerton/internet_fridge/HomeScreen.java
@@ -13,19 +13,20 @@ public class HomeScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 
     @Override
