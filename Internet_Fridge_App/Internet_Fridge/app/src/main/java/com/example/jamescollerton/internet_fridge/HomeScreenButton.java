feabf92b9624379318d8d6a4a4846d7ee17c3074
James Collerton <jamescollerton@live.co.uk>
2016-02-27 22:28:15 +0000

    All cleaned up, ready to dynamically size margin

--- Internet_Fridge_App/Internet_Fridge/app/src/main/java/com/example/jamescollerton/internet_fridge/HomeScreenButton.java
+++ Internet_Fridge_App/Internet_Fridge/app/src/main/java/com/example/jamescollerton/internet_fridge/HomeScreenButton.java
@@ -3,12 +3,6 @@ package com.example.jamescollerton.internet_fridge;
 import android.widget.Button;
 import android.view.View.OnClickListener;
 import android.view.View;
-import android.view.Display;
-import android.view.Window;
-import android.view.WindowManager;
-import android.content.Context;
-import android.util.DisplayMetrics;
-import android.app.Activity;
 
 /**
  * Created by JamesCollerton on 24/02/2016.
@@ -36,17 +30,6 @@ public class HomeScreenButton {
 
     private void setButtonMargins(Button buttonObject){
 
-//        DisplayMetrics metrics = new DisplayMetrics();
-//        ((Activity) getContext()).getWindowManager().getDefaultDisplay().getMetrics(metrics);
-//        int width1 = metrics.widthPixels;
-//        int height1 = metrics.heightPixels;
-
-//        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-//        Display display = wm.getDefaultDisplay();
-//
-//        Display display = getWindowManager().getDefaultDisplay();
-//        int width = display.getWidth();
-//        int height = display.getHeight();
 
 
     }
