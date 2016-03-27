9dff5a267c1f97d5b92e7aeac80cbab3918a5c11
James Collerton <jamescollerton@live.co.uk>
2016-03-27 17:24:01 +0200

    Pushing before I delete what the button used to look like

--- Internet_Fridge_App/Internet_Fridge/app/src/main/java/com/example/jamescollerton/internet_fridge/CreateUserScreen.java
+++ Internet_Fridge_App/Internet_Fridge/app/src/main/java/com/example/jamescollerton/internet_fridge/CreateUserScreen.java
@@ -2,9 +2,11 @@ package com.example.jamescollerton.internet_fridge;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -42,7 +44,6 @@ public class CreateUserScreen extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         setScreenDimensions();
-        setParentScreen();
 
         initialiseText();
         initialiseTextViews();
@@ -50,17 +51,16 @@ public class CreateUserScreen extends AppCompatActivity {
 
     }
 
-
+    /**
+     *
+     * This is used to set the dimensions of the screen according to the Activity input. It takes
+     * them from the extras set on the intent when the intent was started.
+     *
+     */
     private void setScreenDimensions(){
 
         Intent intent = getIntent();
         screenDimensionsMap = (HashMap<String, Integer>)intent.getSerializableExtra(dictionaryKeysList.screenDimensionsMapIntentKey);
-
-    }
-
-    private void setParentScreen(){
-
-        Intent intent = getIntent();
 
     }
 
@@ -71,7 +71,45 @@ public class CreateUserScreen extends AppCompatActivity {
      */
     private void initialiseText(){
 
+        EditText emailEditText = (EditText) findViewById(R.id.settingsScreenEmailTextFieldID);
+        EditText usernameEditText = (EditText) findViewById(R.id.settingsScreenUsernameTextFieldID);
+        EditText passwordEditText = (EditText) findViewById(R.id.settingsScreenPasswordTextFieldID);
 
+        Map<String, EditText> editTextFields = new HashMap<>();
+        editTextFields.put(dictionaryKeysList.settingsScreenUsernameEditTextFieldKey, usernameEditText);
+        editTextFields.put(dictionaryKeysList.settingsScreenPasswordEditTextFieldKey, passwordEditText);
+
+        setEditTextFieldMargins(emailEditText, editTextFields);
+
+    }
+
+    private void setEditTextFieldMargins(EditText topEditTextField, Map<String, EditText> remainingEditTextFields){
+
+        CoordinatorLayout.LayoutParams topEditTextFieldParams = (CoordinatorLayout.LayoutParams) topEditTextField.getLayoutParams();
+
+        double topEditTextFieldTopMargin = screenDimensionsList.settingsScreenEditTextEmailTopPercentageMargin *
+                (double) screenDimensionsMap.get(dictionaryKeysList.screenDimensionsMapScreenHeight);
+
+        double topEditTextFieldSideMargin = screenDimensionsList.settingsScreenEditTextEmailSidePercentageMargin *
+                (double) screenDimensionsMap.get(dictionaryKeysList.screenDimensionsMapScreenWidth);
+
+        topEditTextFieldParams.setMargins((int) topEditTextFieldSideMargin, (int) topEditTextFieldTopMargin, (int) topEditTextFieldSideMargin, 0);
+
+        topEditTextField.setLayoutParams(topEditTextFieldParams);
+
+        int i = 0;
+
+        for (Map.Entry<String, EditText> editTextField: remainingEditTextFields.entrySet()) {
+
+            CoordinatorLayout.LayoutParams remainingEditTextFieldParams = (CoordinatorLayout.LayoutParams) editTextField.getValue().getLayoutParams();
+
+            double editTextFieldTopMargin = topEditTextFieldTopMargin + (++i * screenDimensionsList.settingsScreenEditTextLineSize);
+
+            remainingEditTextFieldParams.setMargins((int) topEditTextFieldSideMargin, (int) editTextFieldTopMargin, (int) topEditTextFieldSideMargin, 0);
+
+            editTextField.getValue().setLayoutParams(remainingEditTextFieldParams);
+
+        }
 
     }
 
@@ -111,6 +149,12 @@ public class CreateUserScreen extends AppCompatActivity {
 
     }
 
+    /**
+     *
+     * This is used to launch the home screen when the user presses the sign me up button and
+     * the user's details are registered and sent off to the server to be processed.
+     *
+     */
     public void launchHomeScreen(){
 
         Intent homeScreenIntent = new Intent(this, HomeScreen.class);
